dd264bb5ab8ba41af075ef998b1ec55c7b05b2de
saneyee sarkar and sravanthi velaga <saneyee@>
2018-01-10 10:03:47 -0800

    all veterinarian model tests are passing

---
+++ Animal_Sanctuary.Tests/ModelTests/VeterinarianTests.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Animal_Sanctuary.Models;
+
+namespace Animal_Sanctuary.Tests
+{
+
+    [TestClass]
+    public class VeterinarianTests
+    {
+        [TestMethod]
+        public void GetName_ReturnsVeterinarianName_String()
+        {
+            //Arrange
+            var veterinarian = new Veterinarian();
+
+
+            //Act
+            var result = veterinarian.Name = "chacko";
+
+            //Assert
+            Assert.AreEqual("chacko", result);
+        }
+
+        [TestMethod]
+        public void GetSpecialty_ReturnsVeterinarianSpecialty_String()
+        {
+            //Arrange
+            var veterinarian = new Veterinarian();
+
+
+            //Act
+            var result = veterinarian.Specialty = "animal Welfare";
+
+            //Assert
+            Assert.AreEqual("animal Welfare", result);
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    }
